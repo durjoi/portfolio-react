6f4faecfc565bb4ff9116500e0922e0546a92675
Mehedi Hassan Durjoi <m.h.durjoi@gmail.com>
2021-11-29 17:53:01 +0600

    Banner layout added

---
+++ src/Pages/Home/Components/Banner/Banner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <section className="grid grid-cols-2 h-auto items-center p-12 border">
+            <div>
+                content
+            </div>
+
+            <div className="flex justify-self-center">
+                image
+            </div>
+        </section>
+    );
+};
+
+export default Banner;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Header from '../../Layouts/Header/Header';
+import Banner from './Components/Banner/Banner';
 
 const Home = () => {
     return (
         <>
-        <Header></Header>
-        <div className="text-sm">
-            This is home
-        </div>
+            <Header></Header>
+            <Banner></Banner>
         </>
     );
 };
